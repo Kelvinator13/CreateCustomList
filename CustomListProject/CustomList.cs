2028927920e5b10052bb8c741e5e038444afccac
kelvin omoregie <kayo2k14@gmail.com>
2020-02-26 13:38:44 -0600

    created the member variables, spawna and variable object from reference from the unit test

--- CustomListProject/CustomList.cs
+++ CustomListProject/CustomList.cs
@@ -23,8 +23,48 @@ namespace CustomListProject
         }
         private class custom<T1>
         {
-            
+            private int number;
+            private int addNumber;
+            private string value;
+            private int removeNumber;
+            private int value1;
+
+            public custom (string value, int number, int addNumber, int removeNumber, int value1)
+            {
+                this.value = value;
+                this.number = number;
+                this.addNumber = addNumber;
+                this.removeNumber = removeNumber;
+                this.value1 = value1;
+            }
+            public int Value
+            {
+                get { return  value; }
+                set { value = number; }
+            }
+            public int Number
+            {
+                get { return number; }
+                set { number = value; }
+            }
+            public int AddNumber
+            {
+                get { return addNumber; }
+                set { addNumber = value; }
+            }
+            public int RemoveNumber
+            {
+                get { return removeNumber; }
+                set { removeNumber = value; }
+            }
+            public int Value1
+            {
+                get { return value1; }
+                set { value1 = value; }
+            }
         }
+
+
     }
 
     }
